3c4f70e43dc72562184aea33a0424c790dc7d3a4
Sydney Barovsky <sbarovsky@gmail.com>
2020-02-27 12:38:07 +0800

    created seed that populated db with 15 drinks, each with 3 ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'open-uri'
+
+puts 'Cleaning database...'
+Cocktail.destroy_all
+Ingredient.destroy_all
+Dose.destroy_all
+
+puts 'Creating new database...'
+# SEED INGREDIENTS W/ JSON LIST
+URL = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+data = JSON.parse(open(URL).read)
+ingredients = []
+data['drinks'].each do |item|
+  ingredients << item['strIngredient1']
+end
+
+15.times do
+  # CREATE COCKTAIL
+  cocktail = Cocktail.new(name: Faker::Dessert.unique.flavor)
+  cocktail.save
+
+  3.times do
+    # CREATE INGREDIENTS
+    ingredient = Ingredient.new(name: ingredients.sample)
+    ingredient.save
+
+    # CREATE DOSE
+    # add corresponding ingredient and cocktail ID
+    dose = Dose.new(
+      description: Faker::Food.measurement,
+      ingredient_id: ingredient.id,
+      cocktail_id: cocktail.id
+    )
+    dose.save
+  end
+
+  cocktail.save!
+end
